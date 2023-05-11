664fd9f000f91f7a7c4f6676ff950ab9fae10cef
Cooper Benson <skycoop@gmail.com>
2023-05-11 11:51:40 -0600

    Setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# PNGme
+
+This is my implementation of the [PNGme](https://picklenerd.github.io/pngme_book/introduction.html) project.
---
+++ src/main.rs
@@ -0,0 +1,11 @@
+use std::error::Error;
+
+mod args;
+mod chunk;
+mod chunk_type;
+mod commands;
+mod png;
+
+fn main() -> Result<(), Box<dyn Error>> {
+    todo!()
+}
